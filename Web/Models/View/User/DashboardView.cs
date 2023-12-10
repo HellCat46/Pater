36020f3f409d8f3199e4795ca51fb21e7c3eb131
HellCat46 <hellcat2388@gmail.com>
2023-12-10 21:38:04 +0530

    Adds Header and Body to User Dashboard

--- Web/Models/View/User/DashboardView.cs
+++ Web/Models/View/User/DashboardView.cs
@@ -1,11 +1,16 @@
+using Web.Models.Account;
 using Web.Models.Link;
 
 namespace Web.Models.View.User;
 
 public class DashboardView
 {
-    public string picPath { get; set; }
+    public string name { get; set; }
+    
+    public AccountModel.Plans plan { get; set; }
+    public string? picPath { get; set; }
     public bool isAdmin { get; set; }
     
     public List<LinkModel> links { get; set; }
 }
+
