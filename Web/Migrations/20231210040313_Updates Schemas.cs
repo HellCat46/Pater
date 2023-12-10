f182b88ac133c20045fd2b561708297fe1f89fcc
HellCat46 <hellcat2388@gmail.com>
2023-12-10 11:20:00 +0530

    oopsie

---
+++ Web/Migrations/20231210040313_Updates Schemas.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatesSchemas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Link",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "Account",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)")
+                .Annotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "Account",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)")
+                .Annotation("Relational:ColumnOrder", 1);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "id",
+                table: "Account",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("Relational:ColumnOrder", 0)
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "createdAt",
+                table: "Account",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isAdmin",
+                table: "Account",
+                type: "bit",
+                nullable: false,
+                defaultValue: false)
+                .Annotation("Relational:ColumnOrder", 5);
+
+            migrationBuilder.AddColumn<string>(
+                name: "name",
+                table: "Account",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("Relational:ColumnOrder", 3);
+
+            migrationBuilder.AddColumn<string>(
+                name: "picPath",
+                table: "Account",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("Relational:ColumnOrder", 4);
+
+            migrationBuilder.CreateTable(
+                name: "ActivityLogsModel",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Userid = table.Column<int>(type: "int", nullable: false),
+                    IPAddr = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Action = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityLogsModel", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_ActivityLogsModel_Account_Userid",
+                        column: x => x.Userid,
+                        principalTable: "Account",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ActivityLogsModel_Userid",
+                table: "ActivityLogsModel",
+                column: "Userid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActivityLogsModel");
+
+            migrationBuilder.DropColumn(
+                name: "createdAt",
+                table: "Account");
+
+            migrationBuilder.DropColumn(
+                name: "isAdmin",
+                table: "Account");
+
+            migrationBuilder.DropColumn(
+                name: "name",
+                table: "Account");
+
+            migrationBuilder.DropColumn(
+                name: "picPath",
+                table: "Account");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "CreatedAt",
+                table: "Link",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "password",
+                table: "Account",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)")
+                .OldAnnotation("Relational:ColumnOrder", 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "Account",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)")
+                .OldAnnotation("Relational:ColumnOrder", 1);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "id",
+                table: "Account",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .Annotation("SqlServer:Identity", "1, 1")
+                .OldAnnotation("Relational:ColumnOrder", 0)
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
