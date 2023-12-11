2450396262701df75d1979e4379bca40c68cbcb2
HellCat46 <hellcat2388@gmail.com>
2023-12-11 16:01:19 +0530

    Adds DashBoard and Logout Controller

--- Web/Controllers/UserController.cs
+++ Web/Controllers/UserController.cs
@@ -1,14 +1,42 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Web.ApplicationDbContext;
+using Web.Models.Account;
+using Web.Models.Link;
+using Web.Models.View.User;
 
 namespace Web.Controllers;
 
 public class UserController : Controller
 {
+    private readonly UserDbContext _context;
+
+    public UserController(UserDbContext context)
+    {
+        _context = context;
+    }
     public IActionResult Dashboard()
     {
-        return View();
+        var SessionID = HttpContext.Session.GetString("SessionID");
+        if (SessionID == null)
+            return RedirectToAction("Index", "Home");
+        
+        AccountModel? Account = _context.Account.FirstOrDefault(model => model.id == Int32.Parse(SessionID));
+        if (Account == null)
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+        return View(new DashboardView()
+        {
+            UserName = Account.name,
+            UserIsAdmin = Account.isAdmin,
+            UserPicPath = Account.PicPath,
+            links = _context.Link.Where(model => model.AccountId == 1 ).ToList()
+        });
     }
+    
     public IActionResult Checkout()
     {
         return View();
@@ -22,5 +50,25 @@ public class UserController : Controller
         return View();
         
     }
+
+
+    public IActionResult Logout()
+    {
+        HttpContext.Session.Clear();
+        return RedirectToAction("Index", "Home");
+    }
     
+    
+    private string GenerateRandom(int len)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        Random random = new Random();
+        String code = "";
+        for (int i = 0; i < len; i++)
+        {
+            code += chars[random.Next(1, chars.Length)];
+        }
+
+        return code;
+    }
 }
