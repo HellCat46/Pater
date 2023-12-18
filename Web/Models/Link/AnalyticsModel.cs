812efe25d20de617683fa055df45efee37db6cf5
HellCat46 <hellcat2388@gmail.com>
2023-12-18 08:39:19 +0530

    Adds Analytics Model

--- Web/Models/Link/AnalyticsModel.cs
+++ Web/Models/Link/AnalyticsModel.cs
@@ -1,6 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Web.Models.Link;
+
 namespace Web.Models;
 
 public class AnalyticsModel
 {
+    [Required]
+    public Device device { get; set; }
     
+    [Required]
+    public OperatingSystem os { get; set; }
+    
+    [Required]
+    public string country { get; set; }
+    
+    [Required]
+    public string city { get; set; }
+    
+    [Required]
+    public DateTime visitedAt { get; set; }
+    
+    
+    [Required]
+    [ForeignKey("LinkCode")]
+    public string LinkCode { get; set; }
+    public LinkModel LinkModel { get; set; }
+
+    public enum Device
+    {
+        Desktop,
+        Mobile,
+        Others
+    }
+
+    public enum OperatingSystem
+    {
+        Linux,
+        Windows,
+        Mac,
+        Android,
+        IOS,
+        Others
+    }
 }
