701ac6781ef2d01010be100f5a45064fd9e20e06
HellCat46 <hellcat2388@gmail.com>
2024-01-09 21:47:50 +0530

    Adds Link Management Options to Details Page

--- Web/wwwroot/js/User/Details.js
+++ Web/wwwroot/js/User/Details.js
@@ -5,6 +5,11 @@ let countryChart;
 let cityChart;
 let deviceChart;
 
+const toastAlert = document.querySelector("#toastAlert");
+const toastAlertType= document.querySelector("#toastAlertType");
+const toastAlertText = document.querySelector("#toastAlertText");
+
+
 async function VisitsChart(actionUrl, code, timeFrame){
     const data= await GetVisitsData(actionUrl, code,timeFrame);
     
@@ -160,7 +165,7 @@ async function CountryChart(actionUrl, code, timeFrame){
         
         tbl.innerHTML = "";
         for(let i =0; i< data.length; i++){
-            tbl.innerHTML += `<tr><th>${i}</th><th>${data[i].label}</th><th>${data[i].data}</th></tr>`;
+            tbl.innerHTML += `<tr><th>${i+1}</th><th>${data[i].label}</th><th>${data[i].data}</th></tr>`;
         }
         return;
     }
@@ -193,22 +198,22 @@ async function CountryChart(actionUrl, code, timeFrame){
 
     tbl.innerHTML = "";
     for(let i =0; i< data.length; i++){
-        tbl.innerHTML += `<tr><th>${i}</th><th>${data[i].label}</th><th>${data[i].data}</th></tr>`;
+        tbl.innerHTML += `<tr><th>${i+1}</th><th>${data[i].label}</th><th>${data[i].data}</th></tr>`;
     }
 }
 async function CityChart(actionUrl, code, timeFrame){
     const data= await GetData(actionUrl,"city", code, timeFrame);
-    //const tbl = document.querySelector("#CityTable")
+    const tbl = document.querySelector("#CityTable")
 
     if(cityChart !== undefined){
         cityChart.data.labels = data.map(d => d.label)
         cityChart.data.datasets[0].data = data.map(d => d.data)
         cityChart.update();
 
-        //tbl.innerHTML = "";
-        //for(let i =0; i< data.length; i++){
-        //    tbl.innerHTML += `<tr><th>${i}</th><th>${data[i].label}</th><th>${data[i].data}</th></tr>`;
-        //}
+        tbl.innerHTML = "";
+        for(let i =0; i< data.length; i++){
+            tbl.innerHTML += `<tr><th>${i+1}</th><th>${data[i].label}</th><th>${data[i].data}</th></tr>`;
+        }
         return;
     }
     
@@ -217,7 +222,7 @@ async function CityChart(actionUrl, code, timeFrame){
         data: {
             labels: data.map(d => d.label),
             datasets: [{
-                label: 'Visitor\'s Country',
+                label: 'Visitor\'s Cities',
                 data: data.map(d => d.data),
                 borderWidth: 1,
                 backgroundColor: "rgb(255, 99, 132)"
@@ -239,17 +244,11 @@ async function CityChart(actionUrl, code, timeFrame){
         }
     });
     
-    //tbl.innerHTML = "";
-    //for(let i =0; i< data.length; i++){
-    //    tbl.innerHTML += `<tr><th>${i}</th><th>${data[i].label}</th><th>${data[i].data}</th></tr>`;
-    //}
+    tbl.innerHTML = "";
+    for(let i =0; i< data.length; i++){
+        tbl.innerHTML += `<tr><th>${i+1}</th><th>${data[i].label}</th><th>${data[i].data}</th></tr>`;
+    }
 }
-
-
-// async function UpdateOtherChart(, actionUrl, type, code, timeFrame){
-//     const data = await  GetData(actionUrl, type, code, timeFrame)
-// }
-
 
 async function GetData(actionUrl, detailType, code, timeFrame) {
     const res = await fetch(`${actionUrl}?detailType=${detailType}&code=${code}&timeFrame=${timeFrame}`, {
@@ -264,4 +263,106 @@ async function GetVisitsData(actionUrl, code, timeFrame) {
     })
 
     return (await res.json());
+}
+
+
+
+function copyCode(baseUrl, code){
+    // This wouldn't work on unsecure connection
+    navigator.clipboard.writeText("https://"+baseUrl+"/"+code).then(() => {
+        toastAlert.className = "toast toast-center";
+        toastAlertType.className = "alert alert-success"
+        toastAlertText.innerText = "Successfully Copied Code to Clipboard";
+    }).catch((err) => {
+        toastAlert.className = "toast toast-center";
+        toastAlertType.className = "alert alert-error"
+        toastAlertType.innerText = "Failed to Copy Code to Clipboard";
+        console.log(err);
+    })
+
+    setTimeout(() => {
+        toastAlert.className = "hidden"
+        toastAlertType.className = "hidden";
+        toastAlertText.innerHTML = "";
+    }, 2000);
+}
+
+
+
+const editLinkCode  = document.querySelector("#editLinkCode");
+const editLinkName = document.querySelector("#editLinkName");
+const editLinkURL = document.querySelector("#editLinkURL");
+const editLinkModal = document.querySelector("#editLink");
+function editLink(code){
+    editLinkName.value = "";
+    editLinkURL.value = "";
+    editLinkCode.value = code;
+    editLinkModal.showModal();
+}
+async function editLinkSubmit(endpointUrl) {
+    editLinkModal.close();
+
+    await fetch(endpointUrl, {
+        method : "PATCH",
+        headers : new Headers({'Content-Type': 'application/json'}),
+        body : JSON.stringify({
+            LinkURL : editLinkURL.value,
+            LinkName: editLinkName.value,
+            LinkCode: editLinkCode.value
+        })
+    }).then(async (res) => {
+        if (res.status === 200){
+            if(editLinkName.value !== "")
+                document.querySelector("#LinkName").innerText = editLinkName.value;
+            if(editLinkURL.value !== ""){
+                document.querySelector("#LinkFavicon").src = `https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=${editLinkURL.value}&size=48`;
+                document.querySelector("#LinkURL").href = editLinkURL.value;
+            }
+            toastAlert.className = "toast toast-center";
+            toastAlertType.className = "alert alert-success"
+            toastAlertText.innerText = "Successfully Updated the Link";
+        }else {
+            toastAlert.className = "toast toast-center";
+            toastAlertType.className = "alert alert-error";
+            const json = await res.json();
+            toastAlertText.innerText = json.error;
+        }
+    }).catch(err => {
+        toastAlert.className = "toast toast-center";
+        toastAlertType.className = "alert alert-error";
+        toastAlertText.innerText = "Unable to Create Link. Please try again later";
+        console.log(err);
+    });
+    setTimeout(()=> {
+        toastAlert.className = "hidden";
+        toastAlertType.className = "hidden";
+        toastAlertText.innerText = "";
+    }, 5000);
+}
+async function deleteLink(endpointUrl, code){
+    const formData = new FormData();
+    formData.append("code", code);
+
+    const res = await fetch(endpointUrl, {
+        method : "DELETE",
+        body : formData
+    }).catch(err => {
+        toastAlert.className = "toast toast-center";
+        toastAlertType.className = "alert alert-error";
+        toastAlertText.innerText = "Unable to Delete Link";
+        console.error(err);
+    });
+    if (res.status === 200){
+        window.location.href = "https://"+window.location.host;
+    }else {
+        toastAlert.className = "toast toast-center";
+        toastAlertType.className = "alert alert-error"
+        const json = await res.json();
+        toastAlertText.innerText = json.error;
+    }
+    setTimeout(() => {
+        toastAlert.className = "hidden"
+        toastAlertType.className = "";
+        toastAlertText.innerText = "";
+    }, 2000);
 }
